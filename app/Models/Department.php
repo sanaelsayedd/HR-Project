847eac9cc8b31bb74bd832e6a940fcce08fb2e12
Sanaelsayedd <sanaelsayed206@gmail.com>
2025-04-19 16:56:27 +0200

    New var
    use superDuper , Update on database , make models & fillables , make all filament resources ,make the Relations in models , put our logo and name

---
+++ app/Models/Department.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Department extends Model
+{
+    protected $fillable = [
+        'Department_Name',
+        'Location',
+
+    ];
+    public function jobPositions()
+{
+    return $this->hasMany(JobPosition::class);
+}
+
+}
