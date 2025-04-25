53052b5a1858e11696cfcd1c1e9e418cdaba530c
end230 <thenaughtyboooy@gmail.com>
2025-04-26 00:38:09 +0300

    routes

--- routes/web.php
+++ routes/web.php
@@ -8,9 +8,7 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
+Route::get('/dashboard', [User_Controller::class, 'index'])->middleware(['auth', 'verified'])->name('dashboard');
 
 Route::middleware('auth')->group(function () {
     // Profile Routes
